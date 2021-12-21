3f96602d2b4669cfb242411902ac0f7a3e991c10
Gianluca Orpello <g.orpello@gmail.com>
2021-12-21 10:21:19 +0100

    Initial Commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 ## CHANGELOG
 
-## Version 1.0.0 (aaaa-mm-dd)
+## Version 1.0.0 (2021-12-21)
 
 First release:
 
-* <Project Name> - <Comment>
+* Present Me - Initial Commit
--- PresentMe/PresentMeView.swift
+++ PresentMe/PresentMeView.swift
@@ -9,35 +9,36 @@ import SwiftUI
 
 struct PresentMeView: View {
     
-    var learner = Learner(name: "Luca", surname: "Palmese", age: 27, imageName: "luca", shortBio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+//    var learner = Learner(name: "Luca", surname: "Palmese", age: 27, imageName: "luca", shortBio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 16.0) {
-            // Horizontally Centered Image
-            HStack {
-                Spacer()
-                Image(learner.imageName)
-                    .resizable()
-                    .frame(width: 200, height: 200)
-                    .clipShape(Circle())
-                    .overlay(
-                        Circle()
-                            .stroke(lineWidth: 8)
-                            .foregroundColor(.yellow)
-                    )
-                    .shadow(radius: 10)
-                    .padding()
-                Spacer()
-            }
-            // Name and Surname (Title)
-            Text("\(learner.name) \(learner.surname)")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundColor(Color.pink)
-            // Description (Body)
-            Text(learner.shortBio)
-        }
-        .padding()
+        Text("Hello World")
+//        VStack(alignment: .leading, spacing: 16.0) {
+//            // Horizontally Centered Image
+//            HStack {
+//                Spacer()
+//                Image(learner.imageName)
+//                    .resizable()
+//                    .frame(width: 200, height: 200)
+//                    .clipShape(Circle())
+//                    .overlay(
+//                        Circle()
+//                            .stroke(lineWidth: 8)
+//                            .foregroundColor(.yellow)
+//                    )
+//                    .shadow(radius: 10)
+//                    .padding()
+//                Spacer()
+//            }
+//            // Name and Surname (Title)
+//            Text("\(learner.name) \(learner.surname)")
+//                .font(.title)
+//                .fontWeight(.bold)
+//                .foregroundColor(Color.pink)
+//            // Description (Body)
+//            Text(learner.shortBio)
+//        }
+//        .padding()
     }
 }
 
